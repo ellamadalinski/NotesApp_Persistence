3b1cd4def84cf1e825cf82552f6a6fc82d82ca26
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-22 07:27:59 -0500

    Initial Commit

---
+++ NotesApp_Persistence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp_Persistence
+//
+//  Created by ELLA MADALINSKI on 10/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
