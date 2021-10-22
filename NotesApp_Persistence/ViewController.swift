2e547ef7af26e80b0337f1741acc50e6a242d54d
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-22 08:09:55 -0500

    class persistence notes done

--- NotesApp_Persistence/ViewController.swift
+++ NotesApp_Persistence/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public struct Contact : Codable{
+    var name : String
+    var age : Int
+}
 
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    var contacts : [Contact] = []
+    @IBOutlet weak var nameOutlet: UITextField!
+    @IBOutlet weak var ageOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
+        contacts.append(Contact(name: "Ella", age: 15))
+        contacts.append(Contact(name: "Olivia", age: 16))
+        
+        if let items = UserDefaults.standard.data(forKey: "myContacts"){
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([Contact].self, from: items){
+                contacts = decoded
+            }
+        }
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row].name
+        cell.detailTextLabel?.text = String(contacts[indexPath.row].age)
+        return cell
+    }
+    
+    
+    @IBAction func addButtonOutlet(_ sender: UIButton) {
+        contacts.append(Contact(name: nameOutlet.text!, age: Int(ageOutlet.text!)!))
+        tableViewOutlet.reloadData()
+    }
+    
+    @IBAction func saveButtonAction(_ sender: UIButton) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(contacts){
+            UserDefaults.standard.set(encoded, forKey: "myContacts")
+        }
+    }
+    
 }
 
